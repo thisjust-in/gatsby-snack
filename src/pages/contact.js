4e9bc0515f227d155035cde8164e6fef87b6d57a
Justin <sunnysidedigitals@gmail.com>
2021-08-04 22:26:14 +0800

    added pages and components + styling

---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Layout from "../components/layout";
+const ContactPage = () => {
+  return (
+    <Layout pageTitle="Contact us">
+      <p>Contact us!</p>
+    </Layout>
+  );
+};
+export default ContactPage;
